b904335e4df6680fd5aa698fbcb23c5c78796087
Aleksey Chaykin <barjastroy@gmail.com>
2024-07-20 14:15:49 +0300

    fix

--- Project.swift
+++ Project.swift
@@ -11,7 +11,7 @@ let project = Project(
             infoPlist: .extendingDefault(
                 with: [
                     "UILaunchScreen": [
-                        "UIColorName": "",
+                        "UIColorName": "Brand",
                         "UIImageName": "",
                     ],
                 ]
--- vkClient/Sources/ContentView.swift
+++ vkClient/Sources/ContentView.swift
@@ -5,7 +5,15 @@ public struct ContentView: View {
     public init() {}
 
     public var body: some View {
-        KFImage(URL(string: "https://cloud.tuist.io/images/tuist_logo_32x32@2x.png")!)
+        VStack {
+            KFImage(URL(string: "https://cloud.tuist.io/images/tuist_logo_32x32@2x.png")!)
+            
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background {
+            VkClientAsset.Colors.brand.swiftUIColor
+                .ignoresSafeArea()
+        }
     }
 }
 
